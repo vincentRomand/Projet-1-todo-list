28a075be1b76efc7c64df5b079e9f2596947a37b
ROMAND Vincent <vincentromand83@gmail.com>
2020-09-18 03:00:04 +0200

    affichage des todos

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-import "./style.css"
+import "./style.css";
+
+const ul = document.querySelector("ul");
+
+const todos = [
+  {
+    text: "je suis une todo",
+    done: false
+  },
+  {
+    text: "faire du javascript",
+    done: true
+  }
+];
+
+const displayTodo = () => {
+  const todosNode = todos.map((todo, index) => {
+    return createTodoElement(todo, index);
+  });
+    ul.innerHTML = "";
+
+   ul.append(...todosNode);
+};
+
+const createTodoElement = (todo, index) => {
+  const li = document.createElement("li");
+  li.innerHTML = `
+    <span class="todo ${todo.done ? "done" : ""}"></span>
+    <p>${todo.text}</p>
+    <button>Editer</button>
+    <button>Supprimer</button>
+  `;
+  return li;
+};
+
+displayTodo();
